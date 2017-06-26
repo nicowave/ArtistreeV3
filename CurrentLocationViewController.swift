d1ae285836acfe718cd6238c12fcae4402a3f23d
nicowave <geoidlife@gmail.com>
2017-06-26 11:50:10 -0600

    added func showLocationServicesDenied to CurrentLocationViewController to alert user to 'Allow location services' to get App to function correctly

--- CurrentLocationViewController.swift
+++ CurrentLocationViewController.swift
@@ -64,5 +64,21 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
 	}
 
 	
+	func showLocationServicesDenied() {
+		
+		let alert = UIAlertController(title: "Location Services Disabled",
+		                              message: "Please enable location services for this app in Settings.",
+		                              preferredStyle: .alert)
+		let okAction = UIAlertAction(title: "OK",
+		                             style: .default,
+		                             handler: nil)
+		
+		alert.addAction(okAction)
+		
+		present(alert, animated: true, completion: nil)
+		
+	}
+	
+	
 }
 
