0cdd6880172c645920f923f33280a1d26b1261c2
nicowave <geoidlife@gmail.com>
2017-06-28 10:13:26 -0600

    added device capabilities gps && location-services to .plist, created LocationsViewController file as UITableViewController embedded new Table View Controller in XIB with Navigation Controller modal segue from GetLocationViewController

---
+++ LocationDetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LocationDetailsViewController.swift
+//  ArtistreeApp
+//
+//  Created by Nicolas Roldos on 6/28/17.
+//  Copyright © 2017 artistreeapp. All rights reserved.
+//
+
+import UIKit
+
+
+class LocationDetailsViewController: UITableViewController {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
